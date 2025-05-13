60b1800e66e624f2dd1fe4c0f312e0f5f8683741
Maariiito <qgmcraft.script@gmail.com>
2025-05-13 09:18:28 -0700

    package update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,16 @@
-// ecosystem.config.js
 module.exports = {
-    apps: [{
-      name: "csv-update-service",
-      script: "./src/scripts/scheduleCSV.js",
-      watch: false,
-      instances: 1,
-      autorestart: true,
-      max_memory_restart: '200M',
-      log_date_format: 'YYYY-MM-DD HH:mm:ss',
-      env: {
-        NODE_ENV: "production",
-      }
-    }]
-  };
+  apps: [{
+    name: "csv-update-service",
+    script: "./src/scripts/scheduleCSV.js",
+    watch: false,
+    instances: 1,
+    exec_mode: "fork",
+    exp_backoff_restart_delay: 100,
+    max_memory_restart: "200M",
+    env: {
+      NODE_ENV: "production"
+    },
+    output: './logs/csv-service-out.log',
+    error: './logs/csv-service-error.log'
+  }]
+}
